714d9c0177057bcb69c32b05549fd5b175578b9d
ghghhhjhjhj <12vivev@skola.lidingo.se>
2026-01-30 19:58:27 +0100

    Add service worker with caching functionality

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install",e=>{
+ e.waitUntil(
+  caches.open("cs-cache").then(c=>
+   c.addAll([
+    "./",
+    "./index.html",
+    "./script.js"
+   ])
+  )
+ )
+})
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+ )
+})
